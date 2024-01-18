7e2d4f515eefbbf1b2754f33a428a4b8b73817ea
Ismail Dalhatu <ismaildalhatu442@gmail.com>
2024-01-18 12:39:01 +0100

    to test

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "https://test-api-tiptopapts-com-3hbx.onrender.com",
+  baseURL: "https://test-api-tiptopapts-com-3hbx.onrender.com/api",
   // baseURL: "http://localhost:1212/api",
   // headers: {
   //   "x-access-token": store.getAccessToken(),
